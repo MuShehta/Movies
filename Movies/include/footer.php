7a5151b4f14591cb795560fc55eb2ce79f95ad85
Mohamed Shehta <52452940+MuShehta@users.noreply.github.com>
2020-12-09 19:22:17 +0200

    Add files via upload

---
+++ Movies/include/footer.php
@@ -0,0 +1,46 @@
+<footer>
+    <div class="row">
+        <div class="section col-4">
+            <h5>الشركة</h5>
+            <hr>
+            <ul>
+                <li>شروط الإستخدام</li>
+                <li>سياسة الخصوصية</li>
+                <li>سياسة ملفات تعريف الإرتباط</li>
+                <li>نموذج الإتصال</li>
+            </ul>
+
+        </div>
+        <div class="section col-4">
+            <h5>أفلام</h5>
+            <hr>
+            <ul>
+                <li>أفلام جديدة</li>
+                <li>جميع الكلمات الدلاية</li>
+                <li>الأفضل من بين الأحدث</li>
+                <li>الأكثر شيوعا</li>
+            </ul>
+
+        </div>
+        <div class="section col-4">
+            <h5>التحديثات</h5>
+            <hr>
+            <ul>
+                <li>blog</li>
+                <li>instgram</li>
+                <li>discord</li>
+                <li>forum</li>
+            </ul>
+
+        </div>
+    </div>
+</footer>
+
+
+
+<script src="admin/layout/js/jquery.js"></script>
+<script src="admin/layout/js/all.min.js"></script>
+<script src="admin/layout/js/bootstrap.bundle.min.js"></script>
+<script src="layout/js/owen.js"></script>
+</body>
+</html>
---
+++ Movies/include/navbar.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+  <a class="navbar-brand" href="index.php">موفز</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav m-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">الرئيسية <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">نبذه عنا</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">الخدمات</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">الشركة</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">تواصل معنا</a>
+      </li>
+    </ul>
+    <form action="index.php" class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" name="search" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">بحث</button>
+    </form>
+  </div>
+</nav>
